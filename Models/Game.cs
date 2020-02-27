a78a808ef37156e2cb3ee066b5fe495d27219de0
Adela Darmansyah and Tiffany Siu <adela@>
2020-02-27 11:26:10 -0800

    Initial boilerplate

---
+++ Models/Game.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace Adventure.Models
+{
+  public class Game
+  {
+    
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace Adventure
+{
+  public class Program
+  {
+    static void Main()
+    {
+
+    }
+  }
+}
