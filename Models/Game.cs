def1515652b6f9094a9383b36b9c9bf79e340213
Adela Darmansyah and Tiffany Siu <adela@>
2020-03-02 11:20:23 -0800

    Separate command switches into separate methods

--- Models/Game.cs
+++ Models/Game.cs
@@ -53,6 +53,9 @@ namespace Adventure.Models
         case 1:
           Console.WriteLine("The ROOM is full of JUNK with banana peels littering the floor.  You see a DOOR that is locked and a friendly looking MONKEY standing in the corner.  Now how to get through that DOOR...");
           break;
+        default:
+          Console.WriteLine("Room number not found!");
+          break;
       }
     }
 
@@ -69,88 +72,16 @@ namespace Adventure.Models
         switch(commands[0])
         {
           case "MOVE":
-            switch(commands[1])
-            {
-              case "DOOR":
-                if (!Door1Locked)
-                {
-                  Console.WriteLine("The unlocked DOOR opens easily and you walk through.");
-                  CurrentRoom = 2;
-                }
-                else
-                {
-                  Console.WriteLine("This DOOR is locked up tight.  I can't open it without a KEY.");
-                }
-                break;
-              default:
-                Console.WriteLine("You try to move but you can't figure out how.");
-                break;
-            }
+            RoomOneMoveCase(commands[1]);
             break; // MOVE case ends
           case "LOOK":
-            switch(commands[1])
-            {
-              case "ROOM":
-                Console.WriteLine("You search the ROOM.  The piles of JUNK and bananas scattered about make it difficult to traverse.  The MONKEY stares at you from his spot in the corner.");
-                break;
-              case "JUNK":
-                Console.WriteLine("You shift through one of the piles of JUNK on the floor and notice a large KEY beneath one of them.");
-                break;
-              case "MONKEY":
-                Console.WriteLine("For being a different species he looks remarkably intelligent. Maybe you can TALK to him?");
-              break;
-              case "KEY":
-                Console.WriteLine("A large KEY. Looks like it should open a DOOR.  Looks important enough anyways.");
-                break;
-              default:
-                Console.WriteLine("You look at the air.  The air stares back...?");
-                break;
-            }
+            RoomOneLookCase(commands[1]);
             break; // LOOK case ends
           case "PICKUP":
-            switch(commands[1])
-            {
-              case "KEY":
-                Console.WriteLine("You pick up the large KEY from the pile of JUNK.  It is surprisingly heavy for its size.");
-                Adventurer.Inventory.Add("HEAVY-KEY");
-                break;
-              case "JUNK":
-                Console.WriteLine("You try to pick up some JUNK but it all looks useless. It'll just take up space in your pocket I guess.");
-                Adventurer.Inventory.Add("JUNK");
-                break;
-              default:
-                Console.WriteLine("You try to pick up the air.  It wasn't interested.");
-                break;
-            } 
+            RoomOnePickupCase(commands[1]);
             break; // PICKUP case ends
           case "TALK":
-            switch(commands[1])
-            {
-              case "MONKEY":
-                Console.WriteLine("'Good day!' says the MONKEY.  'Do you need some HELP?'");
-                Console.WriteLine("How will you reply? (HELP/BYE/INFO)");
-                string reply = Console.ReadLine().ToUpper();
-                switch(reply)
-                {
-                  case "HELP":
-                    Console.WriteLine("'You can LOOK ROOM to look around and you can even combine COMMAND KEYWORD and see what happens!'");
-                    Console.WriteLine("'USE (ITEM-NAME) (OBJECT) to use items from your inventory on an object!'");
-                    break;
-                  case "BYE":
-                    Console.WriteLine("'OK. Bye.'");
-                    break;
-                  case "INFO":
-                    Console.WriteLine("'Well I've been stuck in this ROOM just like you.  Don't know why, but there's plenty of bananas so I'm fine here.'");
-                    break;
-                  default:
-                    Console.WriteLine("'Sorry I don't understand what you're saying.'");
-                    break;
-                }
-                break;
-              default:
-                Console.WriteLine("You talk to yourself.  You wonder what you're doing.");
-                break;
-            } 
+            RoomOneTalkCase(commands[1]); 
             break; // TALK case ends
           case "USE":
             string item = commands[1];
@@ -205,5 +136,96 @@ namespace Adventure.Models
       Console.WriteLine("****************************");
     }
 
+    private void RoomOneMoveCase(string location)
+    {
+      switch(location)
+      {
+        case "DOOR":
+          if (!Door1Locked)
+          {
+            Console.WriteLine("The unlocked DOOR opens easily and you walk through.");
+            CurrentRoom = 2;
+          }
+          else
+          {
+            Console.WriteLine("This DOOR is locked up tight.  I can't open it without a KEY.");
+          }
+          break;
+        default:
+          Console.WriteLine("You try to move but you can't figure out how.");
+          break;
+      }
+    } // RoomOneMoveCase ends
+
+    private void RoomOneLookCase(string item)
+    {
+      switch(item)
+      {
+        case "ROOM":
+          Console.WriteLine("You search the ROOM.  The piles of JUNK and bananas scattered about make it difficult to traverse.  The MONKEY stares at you from his spot in the corner.");
+          break;
+        case "JUNK":
+          Console.WriteLine("You shift through one of the piles of JUNK on the floor and notice a large KEY beneath one of them.");
+          break;
+        case "MONKEY":
+          Console.WriteLine("For being a different species he looks remarkably intelligent. Maybe you can TALK to him?");
+        break;
+        case "KEY":
+          Console.WriteLine("A large KEY. Looks like it should open a DOOR.  Looks important enough anyways.");
+          break;
+        default:
+          Console.WriteLine("You look at the air.  The air stares back...?");
+          break;
+      }
+    } // RoomOneLookCase ends
+
+    private void RoomOnePickupCase(string item)
+    {
+      switch(item)
+      {
+        case "KEY":
+          Console.WriteLine("You pick up the large KEY from the pile of JUNK.  It is surprisingly heavy for its size.");
+          Adventurer.Inventory.Add("HEAVY-KEY");
+          break;
+        case "JUNK":
+          Console.WriteLine("You try to pick up some JUNK but it all looks useless. It'll just take up space in your pocket I guess.");
+          Adventurer.Inventory.Add("JUNK");
+          break;
+        default:
+          Console.WriteLine("You try to pick up the air.  It wasn't interested.");
+          break;
+      } 
+    } // RoomOnePickupCase ends
+
+    private void RoomOneTalkCase(string item)
+    {
+      switch(item)
+      {
+        case "MONKEY":
+          Console.WriteLine("'Good day!' says the MONKEY.  'Do you need some HELP?'");
+          Console.WriteLine("How will you reply? (HELP/BYE/INFO)");
+          string reply = Console.ReadLine().ToUpper();
+          switch(reply)
+          {
+            case "HELP":
+              Console.WriteLine("'You can LOOK ROOM to look around and you can even combine COMMAND KEYWORD and see what happens!'");
+              Console.WriteLine("'USE (ITEM-NAME) (OBJECT) to use items from your inventory on an object!'");
+              break;
+            case "BYE":
+              Console.WriteLine("'OK. Bye.'");
+              break;
+            case "INFO":
+              Console.WriteLine("'Well I've been stuck in this ROOM just like you.  Don't know why, but there's plenty of bananas so I'm fine here.'");
+              break;
+            default:
+              Console.WriteLine("'Sorry I don't understand what you're saying.'");
+              break;
+          }
+          break;
+        default:
+          Console.WriteLine("You talk to yourself.  You wonder what you're doing.");
+          break;
+      }
+    } // RoomOneTalkCase ends
   }
 }
